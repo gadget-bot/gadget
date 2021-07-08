5600e60e004f33ef402ae3d932b886eb78d1496e
Jonathan Gnagy <jonathan.gnagy@gmail.com>
2021-07-07 23:15:00 -0700

    chore: moving eightball to its own plugin

--- main.go
+++ main.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	gadget "github.com/gadget-bot/gadget/core"
-	"github.com/gadget-bot/gadget/plugins/eightball"
 )
 
 func main() {
 	myBot := gadget.Setup()
 
 	// Add your custom plugins here
-	myBot.Router.AddMentionRoutes(eightball.GetMentionRoutes())
 
 	// This launches your bot
 	myBot.Run()
