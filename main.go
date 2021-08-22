71952e9941c950a88d3327a3b2823b7398d9da07
Tim Earle <timearle@protonmail.com>
2021-08-22 14:43:46 -0400

    Bubble up errors and update README

--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"os"
+
 	gadget "github.com/gadget-bot/gadget/core"
 )
 
 func main() {
-	myBot := gadget.Setup()
+	myBot, err := gadget.Setup()
+	if err != nil {
+		println(err.Error())
+		os.Exit(1)
+	}
 
 	// Add your custom plugins here
 
 	// This launches your bot
+
 	myBot.Run()
 }
